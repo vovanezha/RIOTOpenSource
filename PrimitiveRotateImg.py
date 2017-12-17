54db81aa745e3e6d78478d0873cdfcb7018880cb
Vova <vova19tup@gmail.com>
2017-12-17 20:41:14 +0200

    Update PrimitiveRotateImg.py

--- PrimitiveRotateImg.py
+++ PrimitiveRotateImg.py
@@ -2,10 +2,6 @@ import numpy as np
 import argparse 
 import cv2 
 
-# construct the argument parse
-# ap = argparse.ArgumentParser()
-# ap.add_argument("-i", "--image", required=True, help="path to input image file")
-# args = vars(ap.parse_args())
 
 # load the image from disk
 image = cv2.imread('/home/nezha/Изображения/test4.png')
